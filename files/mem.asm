98407bb5f612ca046f2b52cc54008563b27c801e
elneunizar <elneunizar@gmail.com>
2018-04-28 15:13:42 +0700

    review tod 3

---
+++ files/mem.asm
@@ -0,0 +1,22 @@
+segment .data
+    a    dd 4
+    b    dd 4.4
+    c    times 10 dd 0
+    d    dw   1, 2
+    e    db   0xfb
+    f    db   "hello world",0
+
+segment .bss
+    g    resd 1
+    h    resd 10
+    i    resb 100
+
+segment .text
+    global _start
+_start:
+   mov eax,3
+   xor eax,eax
+   mov eax,1
+   int 0x80
+
+
